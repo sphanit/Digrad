9443168fb35f0450155b50116a4821acf418b786
phani <31930181+sphanit@users.noreply.github.com>
2018-04-13 21:51:11 +0530

    Update DiGrad.py

--- DiGrad.py
+++ DiGrad.py
@@ -12,8 +12,8 @@ os.mkdir(s_path)
 f= open(s_path + "/log.txt","w+")
 f.close()
 
-#np.random.seed(1234)
-#tf.set_random_seed(1234)
+np.random.seed(1234)
+tf.set_random_seed(1234)
 
 MAX_EPISODES = 10000
 MAX_EP_STEPS = 180
@@ -48,12 +48,8 @@ with tf.name_scope('A_'):
     A_ = tf.placeholder(tf.float32, shape=[None, ACTION_DIM], name='act_')
 with tf.name_scope('R'):
     R = tf.placeholder(tf.float32, [None, 2], name='r')
-# with tf.name_scope('R2'):
-#     R2 = tf.placeholder(tf.float32, [None, 1], name='r2')
 with tf.name_scope('S_'):
     S_ = tf.placeholder(tf.float32, shape=[None, STATE_DIM], name='s_')
-# with tf.name_scope('S1_'):
-#     S1_ = tf.placeholder(tf.float32, shape=[None, ACTION_DIM], name='s1_')    
 
 
 class Actor2(object):
@@ -83,7 +79,6 @@ class Actor2(object):
 
     def _build_net(self, s, scope, trainable):
         with tf.variable_scope(scope):
-            #init_w = tf.random_normal_initializer(mean=0.0,stddev=1.0)
             init_w = tf.contrib.layers.xavier_initializer()
             init_b = tf.constant_initializer(0.001)
             net = tf.layers.dense(s, 600, kernel_initializer=init_w, bias_initializer=init_b, name='l1', trainable=trainable)
@@ -99,7 +94,6 @@ class Actor2(object):
 
        
             with tf.variable_scope('a'):
-                #w_init = tf.random_uniform_initializer(minval  =-0.003, maxval = 0.003)
                 actions = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, kernel_initializer=init_w,
                                           name='a', trainable=trainable)
                 scaled_a = tf.multiply(actions, self.action_bound, name='scaled_a')  # Scale output to -action_bound to action_bound
@@ -155,8 +149,6 @@ class Actor(object):
                         [self.t_params[i].assign(tf.multiply(self.e_params[i],self.tau) + tf.multiply(self.t_params[i],1-self.tau))
                                                 for i in range(len(self.t_params))]
 
-        # with tf.variable_scope('aa_grad'):
-            # self.aa_grads = tf.gradients(ys=self.a_dep, xs=self.a, grad_ys=a_grad)[0]   # tensor of gradients of each sample (None, a_dim)
 
     def _build_net(self, s, scope, trainable):
         with tf.variable_scope(scope):
@@ -175,14 +167,12 @@ class Actor(object):
             net = tf.nn.dropout(net,0.8)
 
             with tf.variable_scope('a'):
-                #w_init = tf.random_uniform_initializer(minval  =-0.003, maxval = 0.003)
                 actions = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, kernel_initializer=init_w,
                                           name='a', trainable=trainable)
                 scaled_a = tf.multiply(actions, self.action_bound, name='scaled_a')  # Scale output to -action_bound to action_bound
         return scaled_a
 
     def learn(self, s, a):   # batch update
-        #self.sess.run(self.train_op, feed_dict={S: s, A_:a})
         self.sess.run(self.train_op, feed_dict={S: s, A_:a})
         # self.sess.run(self.update_target_params)
 
@@ -221,12 +211,10 @@ class Critic(object):
         with tf.variable_scope('Critic'):
             print 'Critic'
             # Input (s, a), output q
-            # self.a = a
             self.q = self._build_net(S, A_, 'eval_net', trainable=True)
 
             # Input (s_, a_), output q_ for q_target
             self.q_ = self._build_net(S_, A, 'target_net', trainable=False)    # target_q is based on a_ from Actor's target_net
-            # tmp1 = self._build_net_tar(S_, a2_, 'target_net2', trainable=False)    # target_q is based on a_ from Actor's target_net
 
 
             self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval_net')
@@ -247,22 +235,13 @@ class Critic(object):
             self.train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
 
         with tf.variable_scope('a_grad'):
-
-
 	    X = tf.gradients(self.q, A_)[0]
 	    Y = tf.gradients(self.q[:,0], A_)[0]
 	    Z = tf.gradients(self.q[:,1], A_)[0]
-	    #print X
-	    #print tf.concat([tf.gradients(self.q, A1_), tf.gradients(self.q, A2_)],1)
-            #self.a_grads = tf.gradients(self.q, A1_)[0] +  tf.gradients(self.q, A2_)[0] +  tf.gradients(self.q, A3_)[0]   # tensor of gradients of each sample (None, a_dim)
             self.a_grads = tf.concat([tf.concat([0.5*X[:,0:5],Z[:,5:9]],1),Y[:,9:13]],1)   # tensor of gradients of each sample (None, a_dim)
-            #self.a_grads = 0.75*X   # tensor of gradients of each sample (None, a_dim)
-            #self.a_grads = tf.concat(1, [grads,Y[:,5:9]])   # tensor of gradients of each sample (None, a_dim)
-            #self.a_grads = tf.gradients(self.q, A_)[0]   # tensor of gradients of each sample (None, a_dim)
 
     def _build_net(self, s, a, scope, trainable):
         with tf.variable_scope(scope):
-            #init_w = tf.random_normal_initializer(mean=0.0,stddev=1.0)
             init_w = tf.contrib.layers.xavier_initializer()
             init_b = tf.constant_initializer(0.01)
 	    regulate = tf.contrib.layers.l2_regularizer(0.01)
@@ -284,53 +263,6 @@ class Critic(object):
             net = tf.nn.dropout(net,0.8)
 
             with tf.variable_scope('q'):
-		'''
-                w4a = tf.get_variable('w4a', [1800, 300], initializer=init_w, trainable=trainable)
-                b4a = tf.get_variable('b4a', [1, 300], initializer=init_b, trainable=trainable)
-                
-                w4b = tf.get_variable('w4b', [1800, 400], initializer=init_w, trainable=trainable)
-                b4b = tf.get_variable('b4b', [1, 400], initializer=init_b, trainable=trainable)
-
-                w5a = tf.get_variable('w5a', [300, 1], initializer=init_w, trainable=trainable)
-                b5a = tf.get_variable('b5a', [1, 1], initializer=init_b, trainable=trainable)
-            
-                w5b = tf.get_variable('w5b', [400, 2], initializer=init_w, trainable=trainable)
-                b5b = tf.get_variable('b5b', [1, 2], initializer=init_b, trainable=trainable)
-		
-		net_4a = tf.layers.dense(net,300 , activation=tf.nn.crelu,
-                                  kernel_initializer=init_w, bias_initializer=init_b, name='net_4a',
-                                  trainable=trainable, kernel_regularizer = regulate)
-
-		net_4b = tf.layers.dense(net, 400 , activation=tf.nn.crelu,
-                                  kernel_initializer=init_w, bias_initializer=init_b, name='net_4b',
-                                  trainable=trainable, kernel_regularizer = regulate)
-
-
-                #net_4a = tf.nn.crelu(tf.matmul(net, w4a) + b4a)
-                #net_4b = tf.nn.crelu(tf.matmul(net, w4b) + b4b)
-
-
-		value_5a = tf.layers.dense(net_4a,1 ,
-                                  kernel_initializer=init_w, bias_initializer=init_b, name='value_5a',
-                                  trainable=trainable, kernel_regularizer = regulate)
-
-                adv_5b = tf.layers.dense(net_4b, 2,
-                                  kernel_initializer=init_w, bias_initializer=init_b, name='adv_5b',
-                                  trainable=trainable, kernel_regularizer = regulate)
-
-
-		
-
-                #value_5a = tf.identity(tf.matmul(net_4a, w5a) + b5a)
-                #advantage_5b = tf.identity(tf.matmul(net_4b, w5b) + b5b)  
-                # tmp = tf.stack([(advantage_5b - tf.reduce_mean(advantage_5b, axis=1, keep_dims=True)), (advantage_5b - tf.reduce_mean(advantage_5b, axis=1, keep_dims=True))], axis = 0)
-                
-                # print tmp
-
-                q = value_5a +  adv_5b #tf.concat([(advantage_5b - tf.reduce_mean(advantage_5b, axis=1, keep_dims=True)), (advantage_5b - tf.reduce_mean(advantage_5b, axis=1, keep_dims=True))], axis = 0)
-                '''
-                # q = tf.argmax(tmp,1)
-                #w_init = tf.random_uniform_initializer(minval  =-0.003, maxval = 0.003)
                 q = tf.layers.dense(net, 2, kernel_initializer=init_w, bias_initializer=init_b, trainable=trainable)   # Q(s,a)
         return q
 
@@ -366,8 +298,6 @@ class Memory(object):
         return self.data[indices, :]
 
 
-#sess = tf.Session()
-
 config = tf.ConfigProto()
 config.gpu_options.allow_growth=True
 sess = tf.Session(config=config)
@@ -383,9 +313,8 @@ actor.add_grad_to_graph(critic.a_grads)
 M = Memory(MEMORY_CAPACITY, dims=2 * STATE_DIM +  ACTION_DIM + 2)
 
 saver = tf.train.Saver(max_to_keep=0)
-# path = './'+'dpl'
 path  = s_path
-#path = './relu'
+
 if LOAD:
     saver.restore(sess, tf.train.latest_checkpoint(path))
 else:
@@ -394,7 +323,6 @@ else:
 
 def train():
     var = 15.  # control exploration
-    
   
     f = open(s_path + "/log.txt","a+")
     localtime = time.asctime( time.localtime(time.time()) )
@@ -408,24 +336,15 @@ def train():
         stability = 0;
         flag = [0,0]
         for t in range(MAX_EP_STEPS):
-        # while True:
-            #if RENDER:
-             #   env.render()
-
-                # Added exploration noise
-            a1 = actor.choose_action(s)
-            #a1 = np.clip(a1, *ACTION_BOUND);
-            #a2[5:9] = [0,0,0,0];
-            a = a1
+            # Added exploration noise
+            a = actor.choose_action(s)
             a = np.clip(np.random.normal((a), var), *ACTION_BOUND)    # add randomness to action selection for exploration
 	    s_, r, done, dist = env.step(a)
             done1 = done[0]
             done2 = done[1]
 
-
             q_val = critic.ret_q(np.reshape(s,(1,STATE_DIM)),np.reshape(a,(1,ACTION_DIM)))
             M.store_transition(s, a, r, s_)
-            # print a2
 
             if M.pointer > MEMORY_CAPACITY:
                 var = max([var*.999983, VAR_MIN])    # decay the action randomness
@@ -439,28 +358,15 @@ def train():
 
                 actor.learn(b_s,actor.choose_actions(b_s))
 
-
                 qmax += np.max(q[:,0])
                 qmax1 += np.max(q[:,1])
 
-                                
 
-
-		'''	
-                if(q_val[0][0][0] >= np.max(q[:,0])):
-                	flag[0] = 1
-                	r_avg[0] += BETA * delta[0]
-
-                if(q_val[0][0][1] >= np.max(q[:,1])):
-                	flag[1] = 1
-                	r_avg[1] += BETA * delta[1]
-                '''
             s = s_
             ep_reward[0] += r[0]
             ep_reward[1] += r[1]
 
-            if t == MAX_EP_STEPS-1 or (done1 and done2) :
-        # if done:
+            if t == MAX_EP_STEPS-1 or (done1 and done2):
                 if done1 and done2:
                     result = '| done' 
                 elif done1:
@@ -469,13 +375,6 @@ def train():
                     result = '| done2' 
                 else:
                     result =  '| ----'
-
-
-            # if stability == 1:
-            #     result = result +  '|unstable'
-            # else : 
-            #     result = result + '|  stable'
-
 
                 output = 'Ep:'+ str(ep) + result +\
                             '| dist: ' + str(int(dist[0])) + ',' + str(int(dist[1])) + \
@@ -500,27 +399,22 @@ def train():
             save_path = saver.save(sess, ckpt_path, write_meta_graph=False)
             print("\nSave Model %s\n" % save_path)
 
-
-
 def eval():
     count = 101
     while(count):
         i = 0
 	count = count - 1
-        #env.set_fps(30)
         s = env.reset()
         for i in range(1,150):
             i = i + 1
-            #if RENDER:
-             #   env.render()
             a = actor.choose_action(s)
             s_, r, done, dist = env.step(a)
 	    if done[0] and done[1]:
 
 	        break;
             s = s_
-
 	print done[0] and done[1], i, dist[0], dist[1]
+	
 if __name__ == '__main__':
     if LOAD:
         eval()
